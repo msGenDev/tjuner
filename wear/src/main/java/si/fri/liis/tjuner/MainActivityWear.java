f8134cd14443c742d34a3708db4613afe7512015
janm <jan.meznaric@cloud.si>
2015-01-28 01:18:49 +0100

    prvi ispis na uri

--- wear/src/main/java/si/fri/liis/tjuner/MainActivityWear.java
+++ wear/src/main/java/si/fri/liis/tjuner/MainActivityWear.java
@@ -2,12 +2,37 @@ package si.fri.liis.tjuner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivityWear extends Activity {
 
     private TextView mTextView;
+    private Tjuner tjuner;
+    private Handler handler;
+
+    Tjuner.TjunerUIListener listener = new Tjuner.TjunerUIListener(){
+        @Override
+        public void onPitch(String s){
+            //mTextView.setText(s);
+
+            displayData(s);
+
+            Log.e("MAIN", s);
+        }
+    };
+
+    private void displayData(final String s){
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                mTextView.setText(s);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +42,26 @@ public class MainActivityWear extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
+
+                handler = new Handler(Looper.getMainLooper());
+
+
+                tjuner = new Tjuner(listener);
+                tjuner.tune();
                 mTextView = (TextView) stub.findViewById(R.id.text);
+
+
+
             }
         });
 
-        Tjuner tjuner = new Tjuner();
-        tjuner.tune();
+        //mTextView.setText("JAN");
+
+
+
+
+
+
     }
 
 }
--- wear/src/main/java/si/fri/liis/tjuner/Tjuner.java
+++ wear/src/main/java/si/fri/liis/tjuner/Tjuner.java
@@ -6,6 +6,16 @@ import android.media.MediaRecorder;
 import android.os.*;
 import android.util.Log;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
 
 /**
@@ -15,8 +25,11 @@ public class Tjuner {
 
     private final static String LOG_TAG = "Tjuner";
 
+    // pitch string
+    private TjunerUIListener listener;
+
     // audio input parameters
-    private final int SAMPLE_RATE = 8000;
+    private final int SAMPLE_RATE = 16000;
     private final int CHANEL_MODE = AudioFormat.CHANNEL_IN_MONO;
     private final int ENCODING = AudioFormat.ENCODING_PCM_16BIT;
 
@@ -24,14 +37,14 @@ public class Tjuner {
     private AudioRecord input;
 
     // fft window size
-    private final int WINDOW_SIZE_IN_SAMPLES = 8192;
+    private final int WINDOW_SIZE_IN_SAMPLES = SAMPLE_RATE;//8192;
     private final int WINDOW_SIZE_IN_BYTES = 2 * WINDOW_SIZE_IN_SAMPLES;
     private final int WINDOW_SIZE_IN_MS = 1000 * WINDOW_SIZE_IN_SAMPLES / SAMPLE_RATE; // 1000 ms
     private final int WINDOW_SIZE_IN_SHORTS = SAMPLE_RATE * WINDOW_SIZE_IN_MS / 1000;
 
     // min and max tuning frequencies
     private final int MIN_TUNING_FREQUENCY = 100;
-    private final int MAX_TUNING_FREQUENCY = 1000;
+    private final int MAX_TUNING_FREQUENCY = 3000;
 
     // pitch frequency
     private int pitch;
@@ -39,9 +52,10 @@ public class Tjuner {
     // control tuning loop
     private boolean doTune;
 
-    public Tjuner(){
+    public Tjuner(TjunerUIListener listener){
         this.doTune = false;
         this.pitch = 0;
+        this.listener = listener;
     }
 
     public void tune(){
@@ -89,15 +103,28 @@ public class Tjuner {
                     fft.realForward(fft_signal);
 
 
-                    // find maximum frequency
-                    pitch = 0;
-                    for (int i=MIN_TUNING_FREQUENCY; i<=MAX_TUNING_FREQUENCY; i++){
-                        if (fft_signal[i] > fft_signal[pitch]){
-                            pitch = i;
-                        }
-                    }
+                    //fft_signal = lowPassFilter(fft_signal, 10);
 
-                    Log.e(LOG_TAG, "Pitch is " + pitch);
+                    // find maximum frequency
+                    Map peaksMap = findPeaks(fft_signal, MIN_TUNING_FREQUENCY, MAX_TUNING_FREQUENCY, 2, 90);
+                    List peaks = new ArrayList();
+                    peaks.addAll(peaksMap.values());
+                    Collections.reverse(peaks);
+
+
+
+
+                    /*
+                    List<Integer> ext = new ArrayList<Integer>();
+                    for (int i = MIN_TUNING_FREQUENCY; i<MAX_TUNING_FREQUENCY; i++) {
+                        if ((fft_signal[i+1]-fft_signal[i])*(fft_signal[i+2]-fft_signal[i+1]) <= 0) { // changed sign?
+                            ext.add(i+1);
+                        }
+                    }*/
+
+                    // test http://onlinetonegenerator.c
+                    Log.e(LOG_TAG, "Pitch is " + peaks);
+                    listener.onPitch("" + peaks.get(0));
 
                 }
 
@@ -108,6 +135,44 @@ public class Tjuner {
 
                 return null;
             }
+
+
+
         }.execute();
+    }
+
+    private Map<Double, Integer> findPeaks(double[] signal, int min, int max, int n, int k){
+
+        Map<Double, Integer> peaks = new TreeMap<>();
+        int peak = min;
+        for (int i=min; i<=max; i++){
+            if (signal[i] > signal[peak]){
+                peak = i;
+            }
+        }
+        peaks.put(signal[peak], peak);
+        if (n > 1){
+            //if (peak - min > k)
+            //    peaks.putAll(findPeaks(signal, min, peak - k, n - 2, k));
+            peaks.putAll(findPeaks(signal, peak + k, max, n - 1, k));
+        }
+
+
+        return peaks;
+
+    }
+
+    private double[] lowPassFilter(double[] signal, int smoothing){
+        double value = signal[0];
+        for (int i=0; i<signal.length; i++){
+            double curr = signal[i];
+            value += (curr - value) / smoothing;
+            signal[i] = value;
+        }
+        return signal;
+    }
+
+    interface TjunerUIListener {
+        void onPitch(String s);
     }
 }
